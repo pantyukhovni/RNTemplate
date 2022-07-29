31fbecc4536874127758d48a9f46701433456d4d
Никита Пантюхов <nikita.pantyukhov@fogstream.ru>
2022-07-29 16:08:15 +1000

    ui_kit актуализация ветки

--- app/components/WIPSComponent/index.tsx
+++ app/components/WIPSComponent/index.tsx
@@ -3,30 +3,8 @@ import { ScrollView } from 'react-native';
 
 import { Container } from '@app/ui/components/Container';
 import { Text } from '@app/ui/components/Text';
-// import { useDispatch } from 'react-redux';
-
-// import {
-//   getExampleEntitiesSelector,
-//   getExampleIdsSelector,
-// } from '@app/store/example/selectors';
-// import { addTodo } from '@app/store/example/slice';
 
 const WIPComponent = () => {
-  // const todosIds = useSelector(getExampleIdsSelector);
-  // const todosEntitis = useSelector(getExampleEntitiesSelector);
-
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(
-  //     addTodo({
-  //       todoId: '4545',
-  //       text: 'COMPLETED',
-  //       completed: false,
-  //     }),
-  //   );
-  // }, []);
-
   return (
     <Container component={ScrollView}>
       <Text fontSize={14} fontWeight="thin">
